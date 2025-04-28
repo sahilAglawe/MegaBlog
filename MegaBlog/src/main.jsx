dca36d205c84f1fe4375d99cdf2283c4a93c5ff0
SahilAglawe <sahilaglawe66@gmail.com>
2025-04-28 21:32:20 +0530

    Integrate Tailwind CSS for styling, implement authentication with Redux, and set up header and footer components. Update index.html and package files to include Tailwind dependencies.

--- MegaBlog/src/main.jsx
+++ MegaBlog/src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import { createRoot , ReactDOM } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Provider } from 'react-redux'
+import store from './store/store.js'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
     <App />
-  </StrictMode>,
+    </Provider>
+  </React.StrictMode>,
 )
