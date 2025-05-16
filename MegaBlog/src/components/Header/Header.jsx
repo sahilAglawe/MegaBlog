23fc1289498fc3abd8d7a2c10ae11da64c02fd57
SahilAglawe <sahilaglawe66@gmail.com>
2025-05-16 17:26:49 +0530

    Add new components: Container, Logo, and LogoutBtn. Update Header to include navigation items and integrate Redux for authentication. Modify index.js to export new components.

--- MegaBlog/src/components/Header/Header.jsx
+++ MegaBlog/src/components/Header/Header.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import {container,Logo, LogoutBtn} from '../index'
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 function Header(){
+    const authStatus = useSelector((state) => state.auth.status)
+    const navigate = useNavigate()
+
+    const navItems = [
+        {
+           name: 'Home',
+           slug: "/",
+           active: true
+        },{}
+    ]
     return (
-        <div>Header</div>
+        <header className="py-3 shadow bg-gray-500"></header>
     )
 }
 
