4480bbdc10b8d9d9de04d1d75de3f7029cd10433
SahilAglawe <sahilaglawe66@gmail.com>
2025-05-18 17:05:14 +0530

    Add new components: RTE for rich text editing and PostForm for post creation. Integrate react-hook-form for form management in PostForm.

---
+++ MegaBlog/src/components/post-form/PostForm.jsx
@@ -0,0 +1,15 @@
+import React, {useCallback} from "react";
+import { useForm } from "react-hook-form";
+import {Button, Input, Select, RTE} from '../index'
+import appwriteService from "../../appwrite/config";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+function PostForm() {
+    
+    return(
+        <div>test</div>
+    )
+}
+
+export default PostForm
